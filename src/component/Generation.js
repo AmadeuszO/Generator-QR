5451910ce78476ac253053868fdf0e75985e4a4a
AmadeuszO <amadi9205@gmail.com>
2023-11-26 16:38:39 +0100

    add finally files

--- src/component/Generation.js
+++ src/component/Generation.js
@@ -5,25 +5,25 @@ import '../style/generation.css'
 export const Generation = () => {
 
     const [input, setInput] = useState('');
-    const [qeCode, setQrCode] = useState();
+    const [codeQr, setCodeQr] = useState();
     const [load, setLoad] = useState(false);
 
-    const api = ` https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${input}`
-    
     const generationQr = async (e) => {
         e.preventDefault()
 
         try {
             setLoad(true)
-            const resp = await api;
-            setQrCode(resp.url)
-        }catch (err){
+            const resp = await fetch(`https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${input}`);
+            setCodeQr(resp.url)
+        } catch (err) {
             console.log(err)
+        } finally {
+            setLoad(false)
         }
     }
 
     return <>
-        <form className='generation__from'>
+        <form className='generation__from' onSubmit={generationQr}>
             <h1 className='generation__title'>QR Code</h1>
             <input
                 type="text"
@@ -34,11 +34,8 @@ export const Generation = () => {
                 placeholder='URL'
             />
             {load && <div className='generation__load'><span></span>Loading</div>}
-
-            {
-                !load && (qeCode ? <img className="generation__img" src={qeCode} alt='QRCode'/>
-                    : <div className='generation__load'>We use ads to keep our content free for you.</div>)
-            }
+            {!load && (codeQr ? <img className="generation__img" src={codeQr} alt='qe-code'/> :
+                <div className='generation__load'>Generate your QR code</div>)}
             <input type="submit" className='generation__submit' value='Generate QR Code'/>
         </form>
     </>
